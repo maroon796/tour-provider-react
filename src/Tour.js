d390c92f0f68785eb7e007280575b268ba51eec1
Grygory Ksheminsky <maroon785@gmail.com>
2022-05-20 02:49:21 +0300

    working with MoreInfo button

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 const Tour = ({ id, image, info, price, name }) => {
+  const [moreInfo, setMoreInfo] = useState(false);
   return (
     <article className="single-tour">
       <img src={image} alt={name}></img>
@@ -9,7 +10,15 @@ const Tour = ({ id, image, info, price, name }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
-        <p>{info}</p>
+        <p>
+          {moreInfo ? info : `${info.substring(0, 200)}...`}
+          <button
+            onClick={() => {
+              setMoreInfo(!moreInfo);
+            }}>
+            {moreInfo ? 'Show Less' : 'Show More'}
+          </button>
+        </p>
         <button className="delete-btn">Not Interested</button>
       </footer>
     </article>
