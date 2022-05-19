1a9ba7fbe9db29be76287d62f036ba6d86763dac
Grygory Ksheminsky <maroon785@gmail.com>
2022-05-20 02:08:13 +0300

    fetching api

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import Tours from './Tours';
 
 const url = 'https://course-api.com/react-tours-project';
 function App() {
+  const [tours, setTours] = useState([]);
+
+  useEffect(() => {
+    const apiData = async () => {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+    };
+    apiData();
+  });
+
   return <h2>Tours Project Setup</h2>;
 }
 
