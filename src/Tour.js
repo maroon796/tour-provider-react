0b72a35f065420084c90560234b2469c1903459c
Grygory Ksheminsky <maroon785@gmail.com>
2022-05-20 02:41:30 +0300

    finished Tour component

--- src/Tour.js
+++ src/Tour.js
@@ -4,6 +4,14 @@ const Tour = ({ id, image, info, price, name }) => {
   return (
     <article className="single-tour">
       <img src={image} alt={name}></img>
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+        <p>{info}</p>
+        <button className="delete-btn">Not Interested</button>
+      </footer>
     </article>
   );
 };
