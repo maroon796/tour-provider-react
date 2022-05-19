94cfa601ff2c6d88786b70d0176a9e42c966a5f1
Grygory Ksheminsky <maroon785@gmail.com>
2022-05-20 02:14:22 +0300

    working with loading

--- src/App.js
+++ src/App.js
@@ -5,17 +5,31 @@ import Tours from './Tours';
 const url = 'https://course-api.com/react-tours-project';
 function App() {
   const [tours, setTours] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const apiData = async () => {
       const res = await fetch(url);
       const data = await res.json();
+      setLoading(false);
       console.log(data);
     };
     apiData();
-  });
+  }, []);
 
-  return <h2>Tours Project Setup</h2>;
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
 export default App;
